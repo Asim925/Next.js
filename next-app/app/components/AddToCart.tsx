b246df5c8f734f704dd86b692e7b30a72cdffe7b
Asim Nazeer <asimnazeer925@gamil.com>
2024-10-14 16:11:27 +0500

    Routing, fetching the data

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+"use client";
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("clicked")}>Add to cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
