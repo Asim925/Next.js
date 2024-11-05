2ba86f856e3e9be2cad2a95c6e81ce54239cfe36
Asim Nazeer <asimnazeer925@gamil.com>
2024-11-05 16:33:09 +0500

    Making an API

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -9,5 +9,5 @@ interface Props {
 
 export function GET(request: NextRequest, { params: { id } }: Props) {
   if (id > 10) return NextResponse.json({ error: "not found", status: 404 });
-  return NextResponse.json({ id: 1, name: "ali" });
+  return NextResponse.json({ id: 1, name: "alii" });
 }
