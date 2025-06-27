a91e59ebd5e1fc3dd84c05094106bfd50043a99d
Asim Nazeer <asimnazeer925@gamil.com>
2025-06-27 16:28:06 +0500

    theme toggler

---
+++ practice/todo-app/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import ThemeToggler from "./components/ThemeToggler";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Todo-app",
+  description: "Add, delete or edit the tasks",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeToggler />
+        {children}
+      </body>
+    </html>
+  );
+}
