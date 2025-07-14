7ae5c8e7669da6dc1d41fe1ef28bd86801eb3f23
Asim Nazeer <asimnazeer925@gamil.com>
2025-07-14 16:56:08 +0500

    Great changes, see it yourself

--- projects' practice/todo-app/app/page.tsx
+++ projects' practice/todo-app/app/page.tsx
@@ -1,62 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import ApiData, { Data } from "./api/ApiData";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const Page = () => {
-  const [data, setData] = useState<Data[]>([]);
-  let tableHead = ["ID", "User ID", "Title", "Completion", "Delete"];
-
-  let handleDelete = (id: number) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-
-  return (
-    <div>
-      <ApiData setData={setData} />
-
-      <h1 className="text-center w-full mt-10 text-5xl font-extrabold">
-        TODO APP
-      </h1>
-      <div className="m-10 border-gray-500 border-2  flex flex-col gap-2">
-        <table>
-          <thead className="text-left border-b-2 border-gray-400">
-            <tr>
-              {tableHead.map((item, index) => (
-                <th className="p-3" key={index}>
-                  {item}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr className="border-b-2 border-gray-300" key={index}>
-                <td className="p-3">{item.id}</td>
-                <td className="p-3">{item.userId}</td>
-                <td className="p-3">{item.title}</td>
-                <td className="p-3">
-                  {item.completed === true ? "Yes" : "No"}
-                </td>
-                <td className="p-3">
-                  <button
-                    onClick={() => handleDelete(item.id)}
-                    disabled={item.completed === false}
-                    className={`px-4 py-3 bg-red-500 rounded-2xl text-white  transition   ${
-                      item.completed === false
-                        ? "opacity-50 cursor-not-allowed"
-                        : "hover:bg-red-600 hover:scale-104"
-                    }`}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+const page = () => {
+  redirect("/1");
 };
 
-export default Page;
+export default page;
